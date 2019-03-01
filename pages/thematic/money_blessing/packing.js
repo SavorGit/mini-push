b995a8170006005ec78a1e173aeb4db83bc1f7e9
zhangyingtao75144 <125929019@qq.com>
2019-03-01 10:30:57 +0800

    热点投屏小程序3.1.0

--- pages/thematic/money_blessing/packing.js
+++ pages/thematic/money_blessing/packing.js
@@ -71,13 +71,17 @@ Page({
     var re = new RegExp(regu);
     var totalCount = res.detail.value;
     if(totalCount!=''){
-      if (totalCount.search(re) == -1) {
 
+      if ( totalCount.substr(0, 1) == '.') {
+          return '';
+      }
+      if (totalCount.search(re) == -1) {
+        
         totalCount = Math.round(totalCount * 100) / 100;
         if (parseFloat(totalCount).toString() == "NaN") {
           return '';
         }
-        return totalCount;
+        return parseFloat(totalCount).toString() ;
 
       }
       //totalCount = parseFloat(totalCount).toFixed(2);
@@ -88,6 +92,23 @@ Page({
         return 1;
       }
     }
+    /*var obj = res.detail;
+    if (obj.value != '' && obj.value.substr(0, 1) == '.') {
+      obj.value = "";
+    }
+    obj.value = obj.value.replace(/^0*(0\.|[1-9])/, '$1');//解决 粘贴不生效
+    obj.value = obj.value.replace(/[^\d.]/g, "");  //清除“数字”和“.”以外的字符
+    obj.value = obj.value.replace(/\.{2,}/g, "."); //只保留第一个. 清除多余的     
+    obj.value = obj.value.replace(".", "$#$").replace(/\./g, "").replace("$#$", ".");
+    obj.value = obj.value.replace(/^(\-)*(\d+)\.(\d\d).*$/, '$1$2.$3');//只能输入两个小数     
+    if (obj.value.indexOf(".") < 0 && obj.value != "") {//以上已经过滤，此处控制的是如果没有小数点，首位不能为类似于 01、02的金额
+      if (obj.value.substr(0, 1) == '0' && obj.value.length == 2) {
+        obj.value = obj.value.substr(1, obj.value.length);
+      }
+    }
+    console.log(obj.value);
+    return obj.value;*/
+
   },
   //监测发红包个数
   setTotalNums:function(res){
@@ -126,9 +147,7 @@ Page({
   },
   sendRedPacket:function(res){
     var that = this;
-    that.setData({
-      hiddens:false,
-    })
+    
     var totalcount = res.detail.value.totalcount;
     if(totalcount==''){
       wx.showToast({
@@ -189,7 +208,9 @@ Page({
     var rangeid = res.detail.value.rangeid;
     var openid = res.detail.value.openid;
     var box_mnac = res.detail.value.box_mac;
-
+    that.setData({
+      hiddens: false,
+    })
     //发送电视红包
     wx.request({
       url: 'https://mobile.littlehotspot.com/Smallapp3/redpacket/sendTvbonus',
