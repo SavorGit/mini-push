9ee158308ef9ee3332f548a7baca550ce69cd4c4
lz958942 <404644381@qq.com>
2019-07-10 09:37:02 +0800

    文件投屏H5实现 V3.3.1

--- pages/forscreen/forfile/h5files.js
+++ pages/forscreen/forfile/h5files.js
@@ -11,70 +11,90 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    statusBarHeight: getApp().globalData.statusBarHeight,
+    pixelRatio: 0,
+    screenHeight: 0,
+    screenWidth: 0,
+    windowHeight: 0,
+    windowWidth: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
     openid = options.openid;
     box_mac = options.box_mac;
     is_open_simple = options.is_open_simple;
     that.setData({
-      openid : openid,
-      box_mac:box_mac,
-      is_open_simple:is_open_simple
-    })
+      openid: openid,
+      box_mac: box_mac,
+      is_open_simple: is_open_simple
+    });
+    wx.getSystemInfo({
+      success: function(res) {
+        that.setData({
+          pixelRatio: res.pixelRatio,
+          screenHeight: res.screenHeight,
+          screenWidth: res.screenWidth,
+          windowHeight: res.windowHeight,
+          windowWidth: res.windowWidth
+        });
+      }
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
+  },
+
+  pageLoad: function(event) {
+    console.log(event);
   }
 })
