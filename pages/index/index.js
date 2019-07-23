892b7a2d5571b71b42d9864cdf2f1538f9d09b73
lz958942 <404644381@qq.com>
2019-07-23 13:43:32 +0800

    互动页面增加活动促销功能 V3.3.2

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,38 +12,44 @@ Page({
   data: {
     statusBarHeight: getApp().globalData.statusBarHeight,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    openid:'',
-    hotel_name:'',   //酒楼名称
-    room_name:'',    //包间名称
-    box_mac: '',     //机顶盒mac
-    is_link:0,       //是否连接酒楼电视
-    happy_vedio_url: '',          //生日视频url
-    happy_vedio_name: '',          //生日视频名称
-    happy_vedio_title: '',          //生日视频标题
-    showModal: false,   //显示授权登陆弹窗
-    is_game_banner:0,  //是否显示猴子爬树游戏banner
-    is_open_simple:0,
+    openid: '',
+    hotel_name: '', //酒楼名称
+    room_name: '', //包间名称
+    box_mac: '', //机顶盒mac
+    is_link: 0, //是否连接酒楼电视
+    happy_vedio_url: '', //生日视频url
+    happy_vedio_name: '', //生日视频名称
+    happy_vedio_title: '', //生日视频标题
+    showModal: false, //显示授权登陆弹窗
+    showActgoods: false, // 显示活动促销
+    actgoods: { // 活动促销设置
+      showButton4JD: true, // 显示京东购买按钮
+      showButton4Favorites: true, // 显示收藏按钮
+      showFavoritesPanel: true // 显示收藏面板
+    },
+    is_game_banner: 0, //是否显示猴子爬树游戏banner
+    is_open_simple: 0,
     imgUrls: [],
 
-    indicatorDots: true,  //是否显示面板指示点
-    autoplay: true,      //是否自动切换
-    interval: 3000,       //自动切换时间间隔
-    lb_duration: 1000,       //滑动动画时长
+    indicatorDots: true, //是否显示面板指示点
+    autoplay: true, //是否自动切换
+    interval: 3000, //自动切换时间间隔
+    lb_duration: 1000, //滑动动画时长
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     var that = this;
-    if (app.globalData.openid && app.globalData.openid != '') { 
+    if (app.globalData.openid && app.globalData.openid != '') {
       that.setData({
         openid: app.globalData.openid
       })
       openid = app.globalData.openid;
       //判断用户是否注册
       wx.request({
-        url: api_url+'/smallapp21/User/isRegister',
+        url: api_url + '/smallapp21/User/isRegister',
         data: {
           "openid": app.globalData.openid,
           "page_id": 3
@@ -51,7 +57,7 @@ Page({
         header: {
           'content-type': 'application/json'
         },
-        success:function(res){
+        success: function(res) {
           wx.setStorage({
             key: 'savor_user_info',
             data: res.data.result.userinfo,
@@ -62,48 +68,50 @@ Page({
             })
           }
         },
-        fail: function (e) {
+        fail: function(e) {
           wx.setStorage({
             key: 'savor_user_info',
-            data: { 'openid': app.globalData.openid },
+            data: {
+              'openid': app.globalData.openid
+            },
           })
         }
-      });//判断用户是否注册结束
+      }); //判断用户是否注册结束
       wx.request({
-        url: api_url+'/Smallapp/index/isHaveCallBox?openid=' + app.globalData.openid ,
+        url: api_url + '/Smallapp/index/isHaveCallBox?openid=' + app.globalData.openid,
         headers: {
           'Content-Type': 'application/json'
         },
-        success: function (rest) {
+        success: function(rest) {
           var is_have = rest.data.result.is_have;
-          if (is_have == 1) {//已经扫码链接电视
+          if (is_have == 1) { //已经扫码链接电视
             that.setData({
-              is_link:1,
-              hotel_name:rest.data.result.hotel_name,
-              room_name:rest.data.result.room_name,
-              box_mac :rest.data.result.box_mac,
+              is_link: 1,
+              hotel_name: rest.data.result.hotel_name,
+              room_name: rest.data.result.room_name,
+              box_mac: rest.data.result.box_mac,
             })
             box_mac = rest.data.result.box_mac;
             getHotelInfo(rest.data.result.box_mac);
-          }else {
+          } else {
             that.setData({
               is_link: 0,
               box_mac: '',
             })
-            box_mac ='';
+            box_mac = '';
           }
         }
       })
-    }else {
-      app.openidCallback = openid => { 
-        if (openid != '') { 
+    } else {
+      app.openidCallback = openid => {
+        if (openid != '') {
           that.setData({
             openid: openid
           })
           openid = openid;
           //判断用户是否注册
           wx.request({
-            url: api_url+'/smallapp21/User/isRegister',
+            url: api_url + '/smallapp21/User/isRegister',
             data: {
               "openid": app.globalData.openid,
               "page_id": 3
@@ -111,30 +119,32 @@ Page({
             header: {
               'content-type': 'application/json'
             },
-            success: function (res) {
+            success: function(res) {
               wx.setStorage({
                 key: 'savor_user_info',
                 data: res.data.result.userinfo,
               })
-              if (res.data.result.userinfo.is_wx_auth !=3){
+              if (res.data.result.userinfo.is_wx_auth != 3) {
                 that.setData({
                   showModal: true
                 })
               }
-            }, 
-            fail: function (e) {
+            },
+            fail: function(e) {
               wx.setStorage({
                 key: 'savor_user_info',
-                data: { 'openid': openid},
+                data: {
+                  'openid': openid
+                },
               })
             }
-          });//判断用户是否注册结束
+          }); //判断用户是否注册结束
           wx.request({
-            url: api_url+'/Smallapp/index/isHaveCallBox?openid=' + openid ,
+            url: api_url + '/Smallapp/index/isHaveCallBox?openid=' + openid,
             headers: {
               'Content-Type': 'application/json'
             },
-            success: function (rest) {
+            success: function(rest) {
               var is_have = rest.data.result.is_have;
               if (is_have == 1) {
                 that.setData({
@@ -143,9 +153,9 @@ Page({
                   room_name: rest.data.result.room_name,
                   box_mac: rest.data.result.box_mac,
                 })
-                box_mac=rest.data.result.box_mac;
+                box_mac = rest.data.result.box_mac;
                 getHotelInfo(rest.data.result.box_mac);
-              }else {
+              } else {
                 that.setData({
                   is_link: 0,
                   box_mac: '',
@@ -157,9 +167,10 @@ Page({
         }
       }
     }
-    function getHotelInfo(box_mac) {//获取链接的酒楼信息
+
+    function getHotelInfo(box_mac) { //获取链接的酒楼信息
       wx.request({
-        url: api_url+'/Smallapp/Index/getHotelInfo',
+        url: api_url + '/Smallapp/Index/getHotelInfo',
         headers: {
           'Content-Type': 'application/json'
         },
@@ -167,7 +178,7 @@ Page({
           box_mac: box_mac,
         },
         method: "POST",
-        success: function (res) {
+        success: function(res) {
           that.setData({
             hotel_room: res.data.result.hotel_name + res.data.result.room_name,
             happy_vedio_url: res.data.result.vedio_url,
@@ -179,11 +190,11 @@ Page({
       })
     }
     wx.request({
-      url:  api_url+'/Smallapp3/Adsposition/getAdspositionList',
+      url: api_url + '/Smallapp3/Adsposition/getAdspositionList',
       data: {
         position: 2,
       },
-      success: function (res) {
+      success: function(res) {
         if (res.data.code == 10000) {
           var imgUrls = res.data.result;
           that.setData({
@@ -193,17 +204,20 @@ Page({
       }
     })
 
+    this.setData({
+      showActgoods: true
+    });
   },
-  onGetUserInfo: function (res) { 
+  onGetUserInfo: function(res) {
     var that = this;
-    
+
     var user_info = wx.getStorageSync("savor_user_info");
     openid = user_info.openid;
     if (res.detail.errMsg == 'getUserInfo:ok') {
       wx.getUserInfo({
         success(rets) {
           wx.request({
-            url: api_url+'/smallapp3/User/registerCom',
+            url: api_url + '/smallapp3/User/registerCom',
             data: {
               'openid': openid,
               'avatarUrl': rets.userInfo.avatarUrl,
@@ -216,7 +230,7 @@ Page({
             header: {
               'content-type': 'application/json'
             },
-            success: function (res) {
+            success: function(res) {
               if (res.data.code == 10000) {
                 wx.setStorage({
                   key: 'savor_user_info',
@@ -234,8 +248,8 @@ Page({
                 })
               }
 
-            }, 
-            fail: function (res) {
+            },
+            fail: function(res) {
               wx.showToast({
                 title: '微信登陆失败，请重试',
                 icon: 'none',
@@ -245,23 +259,23 @@ Page({
           })
         }
       })
-    }else {
+    } else {
       wx.request({
-        url: api_url+'/smallapp21/User/refuseRegister',
+        url: api_url + '/smallapp21/User/refuseRegister',
         data: {
           'openid': openid,
         },
         header: {
           'content-type': 'application/json'
         },
-        success: function (res) {
+        success: function(res) {
           if (res.data.code == 10000) {
             user_info['is_wx_auth'] = 1;
             wx.setStorage({
               key: 'savor_user_info',
               data: user_info,
             })
-            
+
           } else {
             wx.showToast({
               title: '拒绝失败,请重试',
@@ -273,11 +287,11 @@ Page({
         }
       })
     }
-    
-    
+
+
   },
   //关闭授权弹窗
-  closeAuth:function(){
+  closeAuth: function() {
     //关闭授权登陆埋点
     var that = this;
     that.setData({
@@ -286,11 +300,11 @@ Page({
     var user_info = wx.getStorageSync("savor_user_info");
     openid = user_info.openid;
     console.log(box_mac);
-    if (box_mac == 'undefined' || box_mac == undefined){
+    if (box_mac == 'undefined' || box_mac == undefined) {
       box_mac = '';
     }
     wx.request({
-      url: api_url+'/Smallapp21/index/closeauthLog',
+      url: api_url + '/Smallapp21/index/closeauthLog',
       header: {
         'content-type': 'application/json'
       },
@@ -298,42 +312,42 @@ Page({
         openid: openid,
         box_mac: box_mac,
       },
-      
+
     })
   },
-  
- 
+
+
   //选择照片上电视
   chooseImage(e) {
     var that = this;
     var user_info = wx.getStorageSync("savor_user_info");
-    if (user_info.is_wx_auth!=3){
+    if (user_info.is_wx_auth != 3) {
       that.setData({
-        showModal:true
+        showModal: true
       })
-    }else {
+    } else {
       var box_mac = e.currentTarget.dataset.boxmac;
       var openid = e.currentTarget.dataset.openid;
       var is_open_simple = e.currentTarget.dataset.is_open_simple;
       if (box_mac == '') {
-        
+
         app.scanQrcode();
       } else {
         wx.navigateTo({
           url: '/pages/forscreen/forimages/index?box_mac=' + box_mac + '&openid=' + openid + '&is_open_simple=' + is_open_simple,
         })
-      } 
+      }
     }
   },
   //选择视频投屏
   chooseVedio(e) {
     var that = this
     var user_info = wx.getStorageSync("savor_user_info");
-    if (user_info.is_wx_auth !=3) {
+    if (user_info.is_wx_auth != 3) {
       that.setData({
         showModal: true
       })
-    }else{
+    } else {
       var box_mac = e.currentTarget.dataset.boxmac;
       var openid = e.currentTarget.dataset.openid;
       var is_open_simple = e.currentTarget.dataset.is_open_simple;
@@ -343,20 +357,20 @@ Page({
         wx.navigateTo({
           url: '/pages/forscreen/forvideo/index?box_mac=' + box_mac + '&openid=' + openid + '&is_open_simple=' + is_open_simple,
         })
-      } 
-    } 
+      }
+    }
 
-    
+
   },
-  
-  showHappy(e) {//视频点播让盒子播放
+
+  showHappy(e) { //视频点播让盒子播放
     var box_mac = e.currentTarget.dataset.boxmac;
     var openid = e.currentTarget.dataset.openid;
     if (box_mac == '') {
       app.scanQrcode();
-    }else {
+    } else {
       wx.navigateTo({
-        url: '/pages/thematic/birthday/list?openid='+openid+'&box_mac='+box_mac,
+        url: '/pages/thematic/birthday/list?openid=' + openid + '&box_mac=' + box_mac,
       })
     }
   },
@@ -379,23 +393,23 @@ Page({
 
   },
   //断开连接
-  breakLink: function (e) {
+  breakLink: function(e) {
     var that = this;
     openid = e.currentTarget.dataset.openid;
     box_mac = e.currentTarget.dataset.boxmac;
     var timestamp = (new Date()).valueOf();
     wx.request({
-      url: api_url+'/Smallapp21/index/breakLink',
+      url: api_url + '/Smallapp21/index/breakLink',
       header: {
         'content-type': 'application/json'
       },
       method: "POST",
       data: {
         box_mac: box_mac,
-        openid:openid
+        openid: openid
       },
-      success: function (res) {
-        if(res.data.code==10000){
+      success: function(res) {
+        if (res.data.code == 10000) {
           that.setData({
             is_link: 0,
             box_mac: ''
@@ -408,7 +422,7 @@ Page({
             icon: 'none',
             duration: 2000
           })
-        }else {
+        } else {
           wx.showToast({
             title: '断开失败',
             icon: 'none',
@@ -416,7 +430,7 @@ Page({
           })
         }
       },
-      fail: function (res) {
+      fail: function(res) {
         wx.showToast({
           title: '网络异常，断开失败',
           icon: 'none',
@@ -425,64 +439,64 @@ Page({
       }
     })
   },
-  forscreenHistory:function(e){
+  forscreenHistory: function(e) {
     var box_mac = e.currentTarget.dataset.boxmac;
-    var openid  = e.currentTarget.dataset.openid;
+    var openid = e.currentTarget.dataset.openid;
     if (box_mac == '') {
       app.scanQrcode();
-    }else {
+    } else {
       wx.navigateTo({
-        url: '/pages/forscreen/history/list?openid='+openid+'&box_mac='+box_mac,
+        url: '/pages/forscreen/history/list?openid=' + openid + '&box_mac=' + box_mac,
       })
     }
   },
   //遥控呼大码
-  callQrCode: util.throttle(function (e) {
+  callQrCode: util.throttle(function(e) {
     openid = e.currentTarget.dataset.openid;
     box_mac = e.currentTarget.dataset.box_mac;
     var qrcode_img = e.currentTarget.dataset.qrcode_img;
     app.controlCallQrcode(openid, box_mac, qrcode_img);
-  }, 3000),//呼大码结束
+  }, 3000), //呼大码结束
   //打开遥控器
-  openControl: function (e) {
+  openControl: function(e) {
     var that = this;
-    var qrcode_url = api_url+'/Smallapp/index/getBoxQr?box_mac=' + box_mac + '&type=3';
+    var qrcode_url = api_url + '/Smallapp/index/getBoxQr?box_mac=' + box_mac + '&type=3';
     that.setData({
-      
-      popRemoteControlWindow:true,
+
+      popRemoteControlWindow: true,
       qrcode_img: qrcode_url
     })
   },
   //关闭遥控
-  closeControl: function (e) {
+  closeControl: function(e) {
     var that = this;
     that.setData({
-      
-      popRemoteControlWindow:false,
+
+      popRemoteControlWindow: false,
     })
 
   },
   //遥控退出投屏
-  exitForscreen: function (e) {
+  exitForscreen: function(e) {
     openid = e.currentTarget.dataset.openid;
     box_mac = e.currentTarget.dataset.box_mac;
     app.controlExitForscreen(openid, box_mac);
   },
   //遥控调整音量
-  changeVolume: function (e) {
+  changeVolume: function(e) {
     box_mac = e.currentTarget.dataset.box_mac;
     var change_type = e.currentTarget.dataset.change_type;
     app.controlChangeVolume(box_mac, change_type);
 
   },
   //遥控切换节目
-  changeProgram: function (e) {
+  changeProgram: function(e) {
     box_mac = e.currentTarget.dataset.box_mac;
     var change_type = e.currentTarget.dataset.change_type;
     app.controlChangeProgram(box_mac, change_type);
   },
   //文件投屏
-  forfiles:function(e){
+  forfiles: function(e) {
     var that = this;
     var user_info = wx.getStorageSync("savor_user_info");
     if (user_info.is_wx_auth != 3) {
@@ -494,9 +508,9 @@ Page({
       var openid = e.currentTarget.dataset.openid;
       var is_open_simple = e.currentTarget.dataset.is_open_simple;
       //微信好友文件投屏+h5文件投屏
-      if(box_mac==''){
+      if (box_mac == '') {
         app.scanQrcode();
-      }else {
+      } else {
         that.setData({
           showMe: true,
         })
@@ -511,90 +525,90 @@ Page({
           url: '/pages/forscreen/forfile/files?box_mac=' + box_mac + '&openid=' + openid + "&is_open_simple=" + is_open_simple ,
         })
       }*/
-      
+
     }
   },
   //微信好友文件
-  wxFriendfiles:function(e){
+  wxFriendfiles: function(e) {
     var that = this;
     var box_mac = e.currentTarget.dataset.boxmac;
     var openid = e.currentTarget.dataset.openid;
     var is_open_simple = e.currentTarget.dataset.is_open_simple;
     wx.navigateTo({
       url: '/pages/forscreen/forfile/files?box_mac=' + box_mac + '&openid=' + openid + "&is_open_simple=" + is_open_simple,
-      success:function(e){
-        that.setData({
-          showMe:false
-        })
-      }
-    })
-  },
-  phonefiles:function(e){
-    var that = this;
-    var box_mac = e.currentTarget.dataset.boxmac;
-    var openid = e.currentTarget.dataset.openid;
-    var is_open_simple = e.currentTarget.dataset.is_open_simple;
-    wx.navigateTo({
-      url: '/pages/forscreen/forfile/h5files?box_mac=' + box_mac + '&openid=' + openid + "&is_open_simple=" + is_open_simple,
-      success: function (e) {
+      success: function(e) {
         that.setData({
           showMe: false
         })
       }
     })
   },
-  modalCancel:function(e){
+  phonefiles: function(e) {
+    var that = this;
+    var box_mac = e.currentTarget.dataset.boxmac;
+    var openid = e.currentTarget.dataset.openid;
+    var is_open_simple = e.currentTarget.dataset.is_open_simple;
+    wx.navigateTo({
+      url: '/pages/forscreen/forfile/h5files?box_mac=' + box_mac + '&openid=' + openid + "&is_open_simple=" + is_open_simple,
+      success: function(e) {
+        that.setData({
+          showMe: false
+        })
+      }
+    })
+  },
+  modalCancel: function(e) {
     var that = this;
     that.setData({
-      showMe:false,
+      showMe: false,
     })
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     this.onLoad()
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
