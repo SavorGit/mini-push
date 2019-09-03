b076cb17a08adb041ac53f4e0834f1f3e4eefe62
zhangyingtao75144 <125929019@qq.com>
2019-09-03 15:56:39 +0800

    热点投屏小程序主干版3.5.0

--- pages/demand/goods_detail.js
+++ pages/demand/goods_detail.js
@@ -2,6 +2,7 @@
 const app = getApp()
 var api_url = app.globalData.api_url;
 var box_mac;
+var goods_info;
 Page({
 
   /**
@@ -9,6 +10,7 @@ Page({
    */
   data: {
     statusBarHeight: getApp().globalData.statusBarHeight,
+    goods_info:[]
   },
 
   /**
@@ -31,6 +33,7 @@ Page({
       },
       success: function(res) {
         if (res.data.code == 10000) {
+          goods_info = res.data.result
           that.setData({
             goods_info :res.data.result
           })
@@ -90,11 +93,163 @@ Page({
   onReachBottom: function() {
 
   },
+  //电视播放
+  boxShow: function (e) {
+    console.log(e);
+    var forscreen_id = e.currentTarget.dataset.gods_id;
 
+    var pubdetail = e.currentTarget.dataset.pubdetail;
+    var res_type = 2;
+    var res_nums = 1;
+    app.boxShow(box_mac, forscreen_id, pubdetail, res_type, res_nums);
+  },
+  //收藏资源
+  onCollect: function (e) {
+    var that = this;
+    //var openid = e.target.dataset.openid;
+    var res_id = e.target.dataset.res_id;
+    var user_info = wx.getStorageSync('savor_user_info');
+    var openid = user_info.openid;
+    var res_type = 4;
+    wx.request({
+      url: api_url + '/Smallapp/collect/recLogs',
+      header: {
+        'content-type': 'application/json'
+      },
+      data: {
+        'openid': openid,
+        'res_id': res_id,
+        'type': res_type,
+        'status': 1,
+      },
+      success: function (e) {
+        goods_info.is_collect = 1;
+        goods_info.collect_num = e.data.result.nums;
+        that.setData({
+          goods_info: goods_info
+        })
+        /*if (e.data.code == 10000) {
+          wx.showToast({
+            title: '收藏成功',
+            icon: 'none',
+            duration: 2000
+          })
+        } else {
+          wx.showToast({
+            title: '收藏失败，请稍后重试',
+            icon: 'none',
+            duration: 2000
+          })
+        }*/
+      },
+      fial: function ({ errMsg }) {
+        wx.showToast({
+          title: '网络异常，请稍后重试',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    })
+  },//收藏资源结束
+  //取消收藏
+  cancCollect: function (e) {
+    var that = this;
+    var res_id = e.target.dataset.res_id;
+    var user_info = wx.getStorageSync('savor_user_info');
+    var openid = user_info.openid;
+    var res_type = 4;
+    wx.request({
+      url: api_url + '/Smallapp/collect/recLogs',
+      header: {
+        'content-type': 'application/json'
+      },
+      data: {
+        'openid': openid,
+        'res_id': res_id,
+        'type': res_type,
+        'status': 0,
+      },
+      success: function (e) {
+        goods_info.is_collect=0;
+        goods_info.collect_num = e.data.result.nums;
+        that.setData({
+          goods_info: goods_info
+        })
+        /*if (e.data.code == 10000) {
+          wx.showToast({
+            title: '取消收藏成功',
+            icon: 'none',
+            duration: 2000
+          })
+        } else {
+          wx.showToast({
+            title: '取消收藏失败，请稍后重试',
+            icon: 'none',
+            duration: 2000
+          })
+        }*/
+      },
+      fial: function ({ errMsg }) {
+        wx.showToast({
+          title: '网络异常，请稍后重试',
+          icon: 'none',
+          duration: 2000
+        })
+      }
+    })
+  },//取消收藏结束
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function() {
+  onShareAppMessage: function(res) {
+    var that = this;
+    var user_info = wx.getStorageSync('savor_user_info');
+    var openid = user_info.openid;
+    var res_id = res.target.dataset.res_id;
+    var img_url = res.target.dataset.img_url;
 
+    var goods_id = res.target.dataset.goods_id;
+
+    if (res.from === 'button') {
+      // 转发成功
+      wx.request({
+        url: api_url + '/Smallapp/share/recLogs',
+        header: {
+          'content-type': 'application/json'
+        },
+        data: {
+          'openid': openid,
+          'res_id': goods_id,
+          'type': 4,
+          'status': 1,
+        },
+        success: function (e) {
+          goods_info.share_num ++;
+
+          
+          that.setData({
+            goods_info: goods_info
+          })
+
+        },
+        fail: function ({ errMsg }) {
+          wx.showToast({
+            title: '网络异常，请稍后重试',
+            icon: 'none',
+            duration: 2000
+          })
+        }
+      })
+      // 来自页面内转发按钮
+      return {
+        title: '热点聚焦，投你所好',
+        path: '/pages/demand/goods_detail?goods_id=' + goods_id + '&box_mac=',
+        imageUrl: img_url,
+        success: function (res) {
+
+
+        },
+      }
+    }
   }
 })
