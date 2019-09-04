e3541aba049e16c0d28becc6bfd987047a918a8e
lz958942 <404644381@qq.com>
2019-09-04 11:36:03 +0800

    去除多余数据 V3.5.0

--- pages/find/cards.js
+++ pages/find/cards.js
@@ -195,13 +195,14 @@ Page({
             self.setData({
               cards_img: res.data.result
             })
-          }else {
+          } else {
             self.setData({
-              showContinueModalPopWindow:true
+              showContinueModalPopWindow: true
             })
           }
 
-        },fail:function(res){
+        },
+        fail: function(res) {
           self.setData({
             showContinueModalPopWindow: true
           })
@@ -258,12 +259,12 @@ Page({
   onShareAppMessage: function() {
 
   },
-  goHome:function(e){
+  goHome: function(e) {
     wx.reLaunch({
       url: '/pages/demand/index',
     })
   },
-  getFindList:function(e){
+  getFindList: function(e) {
     var that = this;
     wx.request({
       url: api_url + '/Smallapp3/Find/findlist',
@@ -273,9 +274,9 @@ Page({
       data: {
         openid: openid,
       },
-      success: function (res) {
+      success: function(res) {
         if (res.data.code == 10000) {
-          
+
           that.setData({
             cards_img: res.data.result
           })
@@ -285,7 +286,8 @@ Page({
           })
         }
 
-      }, fail: function (res) {
+      },
+      fail: function(res) {
         that.setData({
           showContinueModalPopWindow: true
         })
@@ -314,7 +316,7 @@ Page({
       })
     } else { //视频
       that.setData({
-        showVideoWindow:true,
+        showVideoWindow: true,
         pop_video_url: pop_video_url
       })
     }
@@ -328,7 +330,7 @@ Page({
     var type = e.currentTarget.dataset.type
     touchEvent["touchEnd"] = touchEvent.pop();
     this.touchMoveHandler.touchMoveHandle(self, touchEvent["touchStart"], touchEvent["touchEnd"], function(handleEvent, page, startEvent, endEvent, top, left, x) {
-      if (handleEvent == self.touchMoveHandler.Event.LeftSlideMoved || handleEvent == self.touchMoveHandler.Event.RightSlideMoved){
+      if (handleEvent == self.touchMoveHandler.Event.LeftSlideMoved || handleEvent == self.touchMoveHandler.Event.RightSlideMoved) {
         wx.request({
           url: api_url + '/Smallapp3/Find/recordViewfind',
           headers: {
@@ -341,7 +343,7 @@ Page({
           },
         })
       }
-      
+
 
       if (handleEvent == self.touchMoveHandler.Event.InsufficientData) {
         var cards_img = self.data.cards_img;
@@ -360,7 +362,7 @@ Page({
           },
           data: {
             openid: openid,
-            find_ids:tmp
+            find_ids: tmp
           },
           success: function(res) {
             if (res.data.code == 10000) {
@@ -370,15 +372,16 @@ Page({
               }
               //self.data.cards_img.push(res.data.result);
 
-            }else {
+            } else {
               self.setData({
                 showContinueModalPopWindow: true,
               })
             }
-          },fail:function(res){
+          },
+          fail: function(res) {
             page_num--;
             self.setData({
-              showContinueModalPopWindow:true,
+              showContinueModalPopWindow: true,
             })
           }
         })
@@ -407,178 +410,16 @@ Page({
       }]
     });
   },
-  // touchMoveHandler: {
-  //   Event: {
-  //     Start: 0x00,
-  //     Less3Item: 0x01,
-  //     UndifindedStartTouchEvent: 0x90,
-  //     UndifindedEndTouchEvent: 0x99,
-  //     LeftSlide: 0x10,
-  //     LeftSlideMoved: 0x19,
-  //     RightSlide: 0x20,
-  //     RightSlideMoved: 0x29,
-  //     ReturnToOrigin: 0x80,
-  //     ReturnToOriginMoved: 0x89
-  //   },
-  //   /**
-  //    * 滑动处理
-  //    *
-  //    * @para page                页面对象
-  //    * @para startEvent          开始滑动的事件
-  //    * @para endEvent            结束滑动的事件
-  //    * @para callbackFunction    执行动画完成后回调函数。返回 Argument{
-  //    *                                                           handleEvent // 处理事件
-  //    *                                                           page // 页面对象
-  //    *                                                           startEvent // 手指滑动开始事件
-  //    *                                                           endEvent // 手指滑动结束事件
-  //    *                                                           top // 元素上边距
-  //    *                                                           left // 元素左边距
-  //    *                                                           x // 元素移动距离
-  //    *                                                      }
-  //    */
-  //   touchMoveHandle: function(page, startEvent, endEvent, callbackFunction) {
-  //     // console.log(page, startEvent, endEvent);
-  //     var handler = this;
-  //     handler.callbackHandel(callbackFunction, handler.Event.Start, page, startEvent, endEvent);
-  //     if (typeof(startEvent) != 'object' || startEvent == null) {
-  //       handler.callbackHandel(callbackFunction, handler.Event.UndifindedStartTouchEvent, page, startEvent, endEvent);
-  //       console.error('start-touch-event is null');
-  //       return;
-  //     }
-  //     if (typeof(endEvent) != 'object' || endEvent == null) {
-  //       handler.callbackHandel(callbackFunction, handler.Event.UndifindedEndTouchEvent, page, startEvent, endEvent);
-  //       console.error('end-touch-event is null');
-  //       return;
-  //     }
-  //     var moveExecuteTrip = touchMoveExecuteTrip / systemInfo.pixelRatio;
-  //     var tripLeft = endEvent.touches[0].pageX - startEvent.touches[0].pageX;
-  //     var tripTop = endEvent.touches[0].pageY - startEvent.touches[0].pageY + systemInfo.statusBarHeight + 46;
-  //     var tripX = endEvent.touches[0].pageX - startEvent.touches[0].pageX;
-  //     var tripY = endEvent.touches[0].pageY - startEvent.touches[0].pageY;
-  //     if (tripX <= -1 * moveExecuteTrip) { // 向左滑动处理
-  //       var x = (systemInfo.screen.width + tripLeft) * -1;
-  //       handler.callbackHandel(callbackFunction, handler.Event.LeftSlide, page, startEvent, endEvent, tripTop, tripLeft, x);
-  //       this.moveOnhorizontalHandel(page, startEvent, endEvent, tripTop, tripLeft, x);
-  //       handler.callbackHandel(callbackFunction, handler.Event.LeftSlideMoved, page, startEvent, endEvent, tripTop, tripLeft, x);
-  //     } else if (tripX >= moveExecuteTrip) { // 向右滑动处理
-  //       var x = systemInfo.screen.width - tripLeft;
-  //       handler.callbackHandel(callbackFunction, handler.Event.RightSlide, page, startEvent, endEvent, tripTop, tripLeft, x);
-  //       this.moveOnhorizontalHandel(page, startEvent, endEvent, tripTop, tripLeft, x);
-  //       handler.callbackHandel(callbackFunction, handler.Event.RightSlideMoved, page, startEvent, endEvent, tripTop, tripLeft, x);
-  //     } else {
-  //       handler.callbackHandel(callbackFunction, handler.Event.ReturnToOrigin, page, startEvent, endEvent, tripTop, tripLeft);
-  //       this.returnToOriginHandel(page, startEvent, endEvent);
-  //       handler.callbackHandel(callbackFunction, handler.Event.ReturnToOriginMoved, page, startEvent, endEvent, tripTop, tripLeft);
-  //     }
-  //     var cards_img = page.data.cards_img;
-  //     cards_img.splice(0, 1);
-  //     page.setData({
-  //       cards_img: cards_img
-  //     });
-  //     if (cards_img.length < 3) {
-  //       handler.callbackHandel(callbackFunction, handler.Event.InsufficientData, page, startEvent, endEvent, tripTop, tripLeft);
-  //     }
-  //   },
-  //   /**
-  //    * 返回原点处理
-  //    *
-  //    * @para page                页面对象
-  //    * @para startEvent          开始滑动的事件
-  //    * @para endEvent            结束滑动的事件
-  //    */
-  //   returnToOriginHandel: function(page, startEvent, endEvent) {
-  //     var animation = wx.createAnimation({
-  //       // duration: 100,
-  //       // timingFunction: 'cubic-bezier(.8,.2,.1,0.8)'
-  //     });
-  //     animation.left(0).top(systemInfo.statusBarHeight + 46).step({
-  //       duration: 100,
-  //       timingFunction: 'ease'
-  //     });
-  //     page.setData({
-  //       animationData: animation.export()
-  //     });
-  //     setTimeout(function() {
-  //       page.setData({
-  //         animationData: {},
-  //         cards: [{
-  //           x: 0,
-  //           y: systemInfo.statusBarHeight + 46
-  //         }]
-  //       });
-  //     }, 100);
-  //   },
-  //   /**
-  //    * 水平移动处理
-  //    *
-  //    * @para page                页面对象
-  //    * @para startEvent          开始滑动的事件
-  //    * @para endEvent            结束滑动的事件
-  //    * @para top                 上边距
-  //    * @para left                左边距
-  //    * @para x                   水平滑动行程
-  //    */
-  //   moveOnhorizontalHandel: function(page, startEvent, endEvent, top, left, x) {
-  //     var animation = wx.createAnimation({
-  //       // duration: 100,
-  //       // timingFunction: 'cubic-bezier(.8,.2,.1,0.8)'
-  //     });
-  //     animation.left(left).top(top).translateX(x).translateY(0).step({
-  //       duration: 300,
-  //       timingFunction: 'linear'
-  //     });
-  //     animation.left(0).top(systemInfo.statusBarHeight + 46).translateX(0).translateY(0).step({
-  //       duration: 0,
-  //       timingFunction: 'step-start'
-  //     });
-  //     page.setData({
-  //       animationData: animation.export()
-  //     });
-  //     setTimeout(function() {
-  //       page.setData({
-  //         animationData: {}
-  //       });
-  //     }, 300);
-  //   },
-  //   /**
-  //    * 回调处理
-  //    *
-  //    * @para callback            执行动画完成后回调函数
-  //    * @para handleEvent         处理事件
-  //    * @para page                页面对象
-  //    * @para startEvent          开始滑动的事件
-  //    * @para endEvent            结束滑动的事件
-  //    * @para top                 上边距
-  //    * @para left                左边距
-  //    * @para x                   水平滑动行程
-  //    */
-  //   callbackHandel: function(callback, handleEvent, page, startEvent, endEvent, top, left, x) {
-  //     if (typeof(callback) != 'function') {
-  //       return;
-  //     }
-  //     callback(handleEvent, page, startEvent, endEvent, top, left, x);
-  //   },
-  //   //电视播放
-  //   boxShow: function(e) {
-  //     var forscreen_id = e.currentTarget.dataset.forscreen_id;
-
-  //     var pubdetail = e.currentTarget.dataset.pubdetail;
-  //     var res_type = e.currentTarget.dataset.res_type;
-  //     var res_nums = e.currentTarget.dataset.res_nums;
-  //     app.boxShow(box_mac, forscreen_id, pubdetail, res_type, res_nums);
-  //   },
-  // },
-
   /**
    * 点击不喜欢
    */
   flyLeft: function(e) {
     var user_info = wx.getStorageSync('savor_user_info');
-    var openid    = user_info.openid;
-    var id        = e.currentTarget.dataset.id;
-    var type      = e.currentTarget.dataset.type;
+    var openid = user_info.openid;
+    var id = e.currentTarget.dataset.id;
+    var type = e.currentTarget.dataset.type;
     var self = this;
-    self.touchMoveHandler.clickMoveHandle(self, self.touchMoveHandler.SlideType.LeftSlide, 675, e, function (handleEvent, page, startEvent, endEvent, top, left, x) {
+    self.touchMoveHandler.clickMoveHandle(self, self.touchMoveHandler.SlideType.LeftSlide, 675, e, function(handleEvent, page, startEvent, endEvent, top, left, x) {
       if (handleEvent == self.touchMoveHandler.Event.LeftSlideMoved) {
         wx.request({
           url: api_url + '/Smallapp3/Find/recordViewfind',
@@ -596,8 +437,8 @@ Page({
         var cards_img = self.data.cards_img;
         var tmp = '[';
         var space = '';
-        for(var i=0;i<cards_img.length;i++){
-          tmp += space +'{"id":'+cards_img[i].id+',"type":'+cards_img[i].type+'}';
+        for (var i = 0; i < cards_img.length; i++) {
+          tmp += space + '{"id":' + cards_img[i].id + ',"type":' + cards_img[i].type + '}';
           space = ',';
         }
         tmp += ']';
@@ -610,7 +451,7 @@ Page({
             openid: openid,
             find_ids: tmp
           },
-          success: function (res) {
+          success: function(res) {
             if (res.data.code == 10000) {
               var list_info = res.data.result;
               for (var k = 0; k < list_info.length; k++) {
@@ -623,7 +464,8 @@ Page({
                 showContinueModalPopWindow: true,
               })
             }
-          }, fail: function (res) {
+          },
+          fail: function(res) {
 
             self.setData({
               showContinueModalPopWindow: true,
@@ -631,7 +473,7 @@ Page({
           }
         })
       }
-      
+
       //console.log(handleEvent, page, startEvent, endEvent, top, left, x);
     });
   },
@@ -645,14 +487,14 @@ Page({
     var id = e.currentTarget.dataset.id;
     var type = e.currentTarget.dataset.type;
     var forscreen_id = e.currentTarget.dataset.forscreen_id;
-    if (type == 2 || type==3) {
+    if (type == 2 || type == 3) {
       var res_id = forscreen_id;
       var c_type = 2;
     } else {
       var res_id = id;
       var c_type = 3
     }
-    self.touchMoveHandler.clickMoveHandle(self, self.touchMoveHandler.SlideType.RightSlide, 675, e, function (handleEvent, page, startEvent, endEvent, top, left, x) {
+    self.touchMoveHandler.clickMoveHandle(self, self.touchMoveHandler.SlideType.RightSlide, 675, e, function(handleEvent, page, startEvent, endEvent, top, left, x) {
       console.log("handleEvent=" + handleEvent)
       if (handleEvent == self.touchMoveHandler.Event.RightSlideMoved) {
         wx.request({
@@ -666,7 +508,7 @@ Page({
             type: type,
           },
         })
-        
+
         wx.request({
           url: api_url + '/Smallapp/collect/recLogs',
           header: {
@@ -698,7 +540,7 @@ Page({
             openid: openid,
             find_ids: tmp
           },
-          success: function (res) {
+          success: function(res) {
             if (res.data.code == 10000) {
               var list_info = res.data.result;
               for (var k = 0; k < list_info.length; k++) {
@@ -711,8 +553,9 @@ Page({
                 showContinueModalPopWindow: true,
               })
             }
-          }, fail: function (res) {
-            
+          },
+          fail: function(res) {
+
             self.setData({
               showContinueModalPopWindow: true,
             })
@@ -723,7 +566,7 @@ Page({
     });
   },
   //电视播放
-  boxShow: function (e) {
+  boxShow: function(e) {
     console.log(e);
     var forscreen_id = e.currentTarget.dataset.forscreen_id;
 
@@ -733,13 +576,13 @@ Page({
     app.boxShow(box_mac, forscreen_id, pubdetail, res_type, res_nums);
   },
   //点击分享按钮
-  onShareAppMessage: function (res) {
+  onShareAppMessage: function(res) {
     var that = this;
     var user_info = wx.getStorageSync('savor_user_info');
     var openid = user_info.openid;
-    var type   = res.target.dataset.type;
+    var type = res.target.dataset.type;
     var res_type = res.target.dataset.res_type;
-    if(type==1){
+    if (type == 1) {
       var res_id = res.target.dataset.id;
       var c_type = 3;
       if (res_type == 1) {
@@ -747,23 +590,23 @@ Page({
       } else {
         var share_url = '/pages/share/video?res_id=' + res_id + '&type=3';
       }
-      
-    }else if(type==2 || type==3){
+
+    } else if (type == 2 || type == 3) {
       var res_id = res.target.dataset.forscreen_id;
       var c_type = 2;
-      if (res_type==1){
+      if (res_type == 1) {
         var share_url = '/pages/share/pic?forscreen_id=' + res_id;
 
-      }else {
+      } else {
         var share_url = '/pages/share/video?res_id=' + res_id + '&type=2';
       }
     }
     var video_url = res.target.dataset.video_url;
-    var img_url   = res.target.dataset.img_url;
-    var res_url   = res.target.dataset.res_url;
+    var img_url = res.target.dataset.img_url;
+    var res_url = res.target.dataset.res_url;
 
     if (res.from === 'button') {
-      
+
       // 转发成功
       wx.request({
         url: api_url + '/Smallapp/share/recLogs',
@@ -776,13 +619,15 @@ Page({
           'type': c_type,
           'status': 1,
         },
-        success: function (e) {
+        success: function(e) {
           //var cards_img = that.cards_img
-          
-          
+
+
 
         },
-        fail: function ({ errMsg }) {
+        fail: function({
+          errMsg
+        }) {
           wx.showToast({
             title: '网络异常，请稍后重试',
             icon: 'none',
@@ -795,40 +640,40 @@ Page({
         title: '热点聚焦，投你所好',
         path: share_url,
         imageUrl: img_url,
-        success: function (res) {
+        success: function(res) {
 
 
         },
       }
     }
-  },// 分享结束
-  closePopVideo:function(e){
+  }, // 分享结束
+  closePopVideo: function(e) {
     var that = this;
     that.setData({
-      showVideoWindow:false
+      showVideoWindow: false
     })
   },
-  forDetail:function(e){
+  forDetail: function(e) {
     var box_mac = e.currentTarget.dataset.box_mac;
     var forscreen_id = e.currentTarget.dataset.forscreen_id;
     var goods_id = e.currentTarget.dataset.goods_id;
-    var type     = e.currentTarget.dataset.type;   //1官方 2精选 3公开
+    var type = e.currentTarget.dataset.type; //1官方 2精选 3公开
     var res_type = e.currentTarget.dataset.res_type; //1图片2视频
     var path = '';
-    if(type==1){
+    if (type == 1) {
       var video_url = e.currentTarget.dataset.res_url;
-      var title = encodeURIComponent(e.currentTarget.dataset.title)  ;
+      var title = encodeURIComponent(e.currentTarget.dataset.title);
       var file_name = e.currentTarget.dataset.filename;
-      var img_url   = e.currentTarget.dataset.img_url;
-      path = '/pages/forscreen/video/launch_video?video_url=' + video_url + '&video_name=' + title + '&box_mac=' + box_mac + '&res_id=' + goods_id + '&filename=' + file_name+'&video_img_url='+img_url;
+      var img_url = e.currentTarget.dataset.img_url;
+      path = '/pages/forscreen/video/launch_video?video_url=' + video_url + '&video_name=' + title + '&box_mac=' + box_mac + '&res_id=' + goods_id + '&filename=' + file_name + '&video_img_url=' + img_url;
       console.log(path)
-    }else {
-      if(res_type==1){//图片
+    } else {
+      if (res_type == 1) { //图片
         path = '/pages/find/picture?forscreen_id=' + forscreen_id + '&box_mac=' + box_mac;
-      }else {//视频
+      } else { //视频
         path = '/pages/find/video?forscreen_id=' + forscreen_id + '&box_mac=' + box_mac;
       }
-      
+
     }
     console.log(path);
     wx.navigateTo({
