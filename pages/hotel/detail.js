60db83b8bf7a4e6f661015490a596399e9bf58c9
zhangyingtao75144 <125929019@qq.com>
2020-02-13 12:14:45 +0800

    热点投屏主干版4.3.0

--- pages/hotel/detail.js
+++ pages/hotel/detail.js
@@ -1,4 +1,10 @@
 // pages/hotel/detail.js
+const app = getApp()
+const utils = require('../../utils/util.js')
+const mta = require('../../utils/mta_analysis.js')
+var api_url = app.globalData.api_url;
+var merchant_id;
+var page = 1;
 Page({
 
   /**
@@ -12,9 +18,54 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
+    if (typeof (options.q) != 'undefined') {
+      var q = decodeURIComponent(options.q);
+      var selemite = q.indexOf("?");
+      var pams = q.substring(selemite + 3, q.length);
 
+      var pams_arr = pams.split('_');
+      merchant_id = pams_arr[1];
+    }else {
+      merchant_id = options.merchant_id;
+    }
+    
+    //商家详情
+    utils.PostRequest(api_url + '/aa/bb/cc', {
+      merchant_id: merchant_id,
+    }, (data, headers, cookies, errMsg, statusCode) => self.setData({
+      hotel_info: data.result
+    }));
+
+    //菜品列表
+    utils.PostRequest(api_url + '/aa/bb/cc', {
+      merchant_id: merchant_id,
+      page:1
+    }, (data, headers, cookies, errMsg, statusCode) => self.setData({
+      dishes_list: data.result
+    }));
   },
-
+  /**
+   * 拨打订餐电话
+   */
+  phonecallevent: function (e) {
+    var tel = e.target.dataset.tel;
+    wx.makePhoneCall({
+      phoneNumber: tel
+    })
+    
+  },
+  loadMore:function(e){
+    var that = this;
+    page +=1;
+    //菜品列表
+    utils.PostRequest(api_url + '/aa/bb/cc', {
+      merchant_id: merchant_id,
+      page: page
+    }, (data, headers, cookies, errMsg, statusCode) => self.setData({
+      dishes_list: data.result
+    }));
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -60,7 +111,19 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function (e) {
+    var img_url = e.currentTarget.dataset.img_url;
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      return {
+        title: '热点聚焦，投你所好',
+        path: '/pages/hotel/index?merchant_id=' + merchant_id,
+        imageUrl: img_url,
+        success: function (res) {
 
+
+        },
+      }
+    }
   }
 })
