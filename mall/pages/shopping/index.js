173d1ec0e9bdaf91cf767ed7100f9a384a675903
lz958942 <404644381@qq.com>
2020-04-06 20:51:39 +0800

    1、增加商城首页 2、增加商品搜索页 3、增加商品详情页面

---
+++ mall/pages/shopping/index.js
@@ -0,0 +1,95 @@
+// mall/pages/shopping/index.js
+/**
+ * 【商城】首页
+ */
+
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    SystemInfo: getApp().SystemInfo,
+    statusBarHeight: getApp().globalData.statusBarHeight,
+    topBanners: {
+      indicatorDots: true,
+      autoplay: true,
+      interval: 3000,
+      duration: 300,
+      list: [
+        {
+          id: '001',
+          // title: '广告01',
+          pic: 'https://oss.littlehotspot.com/WeChat/MiniProgram/LaunchScreen/source/images/imgs/default.jpeg'
+        },
+        {
+          id: '001',
+          // title: '广告02',
+          pic: 'https://oss.littlehotspot.com/WeChat/MiniProgram/LaunchScreen/source/images/imgs/default.jpeg'
+        },
+        {
+          id: '001',
+          title: '广告03',
+          pic: 'https://oss.littlehotspot.com/WeChat/MiniProgram/LaunchScreen/source/images/imgs/default.jpeg'
+        }
+      ]
+    }
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
