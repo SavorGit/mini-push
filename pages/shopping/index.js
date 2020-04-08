25406bde7c84a3d8d2203d37013a89df74f99a4c
zhangyingtao75144 <125929019@qq.com>
2020-04-08 12:09:10 +0800

    主干版4.4.0-商城

--- pages/shopping/index.js
+++ pages/shopping/index.js
@@ -55,7 +55,11 @@ Page({
       interval: 3000,
       duration: 300,
       list:[]
-    }
+    },
+    box_mac:'',
+    page_arr: [],
+    category_id: 0,
+    keywords: ''
   },
 
   /**
@@ -79,19 +83,37 @@ Page({
         }
       }
     }
-    var user_info = wx.getStorageSync("savor_user_info");
+    
     //轮播图
+    self.getBannerList();
+    //获取商品分类
+    self.getCategoryList();
+    //商品列表
+    self.getGoodsList(0, 1);
+  },
+  isHaveCallBox:function(openid){
+    var that = this;
+    utils.PostRequest(api_url + '/Smallapp4/index/isHaveCallBox?openid=' + openid, {}, (data, headers, cookies, errMsg, statusCode) => {
+      var is_have = data.result.is_have;
+      if (is_have == 1) {
+        self.setData({
+          box_mac: data.result.box_mac,
+          hotel_info: data.result,
+        });
+      }
+    })
+  },
+  //轮播图
+  getBannerList:function(){
+    var self = this;
     utils.PostRequest(api_v_url + '/Adsposition/getAdspositionList', {
       position: 1,
     }, (data, headers, cookies, errMsg, statusCode) => {
       var topBanners = self.data.topBanners;
-      //topBanners.list.push(data.result);
       topBanners.list = data.result
-      console.log(topBanners);
       self.setData({
         topBanners: topBanners,
       })
-      //self.data.topBanners.list = data.result
     });
   },
   //banner点击跳转
@@ -102,6 +124,150 @@ Page({
     })*/
     wx.switchTab({
       url: linkcontent
+    })
+  },
+  //商品分类
+  getCategoryList: function () {
+    var that = this;
+    utils.PostRequest(api_v_url + '/category/categorylist', {
+    }, (data, headers, cookies, errMsg, statusCode) => {
+      var category_list = data.result.category_list
+      var page_arr = that.data.page_arr;
+      for (var i = 0; i < category_list.length; i++) {
+        var id = category_list[i].id;
+        page_arr[id] = 1;
+      }
+      that.setData({
+        page_arr: page_arr,
+        category_list: category_list,
+      })
+    });
+  },
+  //商品列表
+  getGoodsList: function (category_id, page) {
+    var that = this;
+    utils.PostRequest(api_v_url + '/shop/goods', {
+      category_id: category_id,
+      openid: openid,
+      page: page
+    }, (data, headers, cookies, errMsg, statusCode) => {
+      that.setData({
+        goods_list: data.result.datalist,
+      })
+    });
+  },
+  //选择分类
+  selectCate: function (e) {
+    var that = this;
+    var category_id = e.currentTarget.dataset.category_id;
+    that.setData({
+      category_id: category_id
+    })
+    var page_arr = that.data.page_arr;
+    var select_page = 1;
+    for (let index in page_arr) {
+      if (category_id == index) {
+        select_page = page_arr[index];
+      }
+    }
+    that.getGoodsList(category_id, select_page);
+  },
+  loadMore: function (e) {
+    var that = this;
+    var category_id = that.data.category_id;
+    var page_arr = that.data.page_arr;
+    var select_page = 1;
+    for (let index in page_arr) {
+      if (index == category_id) {
+        select_page = page_arr[index] + 1;
+        page_arr[index] += 1;
+        break;
+      }
+    }
+    that.setData({
+      page_arr: page_arr,
+    })
+    that.getGoodsList(category_id, select_page)
+  },
+  //电视播放
+  boxShow(e) {
+    let self = this;
+    let listIndex = e.currentTarget.dataset.index;
+    var box_mac = e.target.dataset.boxmac;
+
+
+    if (box_mac == '') {
+      app.scanQrcode(pageid);
+    } else {
+      var openid = e.currentTarget.dataset.openid;
+      var vediourl = e.currentTarget.dataset.vediourl;
+      var forscreen_char = e.currentTarget.dataset.name;
+      var filename = e.currentTarget.dataset.filename; //文件名
+      var timestamp = (new Date()).valueOf();
+      var mobile_brand = app.globalData.mobile_brand;
+      var mobile_model = app.globalData.mobile_model;
+
+
+      var forscreen_id = e.currentTarget.dataset.goods_id;
+      var res_type = 2;
+      var res_nums = 1;
+      var duration = e.currentTarget.dataset.duration;
+      var resource_size = e.currentTarget.dataset.resource_size;
+      var forscreen_url = e.currentTarget.dataset.forscreen_url;
+      var filename = e.currentTarget.dataset.filename;
+      var tx_url = e.currentTarget.dataset.tx_url;
+      var pubdetail = [{
+        'duration': 0,
+        'resource_size': 0,
+        'forscreen_url': '',
+        'res_id': 0
+      }];
+      for (var i = 0; i < 1; i++) {
+        pubdetail[0].duration = duration;
+        pubdetail[0].resource_size = resource_size;
+        pubdetail[0].forscreen_url = forscreen_url;
+        pubdetail[0].res_id = forscreen_id;
+        pubdetail[0].filename = filename;
+        pubdetail[0].res_url = tx_url;
+      }
+      var hotel_info = e.currentTarget.dataset.hotel_info;
+      app.boxShow(box_mac, forscreen_id, pubdetail, res_type, res_nums, 5, hotel_info, self);
+
+      // 调用记录播放次数接口
+      utils.PostRequest(api_url + '/Smallapp4/demand/recordPlaynum', {
+        openid: openid,
+        res_id: forscreen_id
+      }, (data, headers, cookies, errMsg, statusCode) => {
+        let program_list = self.data.program_list;
+        program_list[listIndex].play_num = data.result.play_num;
+        self.setData({
+          program_list: program_list
+        });
+      });
+
+      mta.Event.stat('optimizationClickTvPlay', { 'goodsid': forscreen_id })
+
+    }
+  }, //电视播放结束
+
+
+  //查看详情
+  gotoDetail: function (e) {
+    var goods_id = e.currentTarget.dataset.goods_id;
+    var box_mac = e.currentTarget.dataset.box_mac;
+    var type = e.currentTarget.dataset.type;
+    console.log(e);
+    return false;
+    if(type==10){
+      var url = '/pages/demand/goods_detail?goods_id=' + goods_id + '&box_mac=' + box_mac
+      wx.navigateTo({
+        url: '/pages/demand/goods_detail?goods_id=' + goods_id + '&box_mac=' + box_mac,
+      })
+    }else if(type==22){
+      var url = '/mall/pages/goods/detail?goods_id' + goods_id;
+    }
+    wx.navigateTo({
+      url: url,
     })
   },
   /**
@@ -115,7 +281,10 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this;
+    var user_info = wx.getStorageSync("savor_user_info");
+    //判断是否连接包间盒子
+    that.isHaveCallBox(user_info.openid);
   },
 
   /**
