754ba5ee234d5ff1f8e530d0eb997b6b15be9353
zhangyingtao75144 <125929019@qq.com>
2020-05-20 17:10:04 +0800

    主干版4.4.2-商品买赠

--- pages/shopping/index.js
+++ pages/shopping/index.js
@@ -111,7 +111,7 @@ Page({
     utils.PostRequest(api_url + '/Smallapp4/index/isHaveCallBox?openid=' + openid, {}, (data, headers, cookies, errMsg, statusCode) => {
       var is_have = data.result.is_have;
       if (is_have == 1) {
-        self.setData({
+        that.setData({
           box_mac: data.result.box_mac,
           hotel_info: data.result,
         });
