aacd71070f763212951307e7c890f258e4ceecfc
zhangyingtao75144 <125929019@qq.com>
2020-08-18 18:33:43 +0800

    主干版4.6.4

--- pages/forscreen/forimages/index.js
+++ pages/forscreen/forimages/index.js
@@ -49,6 +49,7 @@ Page({
     res_head_desc:'图片',
     launchType:'classic',
     wifi_hidden: true,
+    hiddens: true, //
   },
   /**
    * 生命周期函数--监听页面加载
@@ -245,6 +246,8 @@ Page({
       
 
       that.setData({
+        load_fresh_char: '亲^_^投屏中,请稍后...',
+        hiddens: false,
         is_btn_disabel: true,
         is_share: is_share,
         is_assist:is_assist
@@ -283,6 +286,9 @@ Page({
                   
                   
                 } else if (res.cancel) {
+                  that.setData({
+                    hiddens: true,
+                  })
                   wx.navigateBack({
                     delta:1
                   })
@@ -325,6 +331,8 @@ Page({
       console.log(up_imgs);
       //return false;
       that.setData({
+        load_fresh_char: '亲^_^投屏中,请稍后...',
+        hiddens: false,
         is_btn_disabel: true,
         
       })
@@ -359,6 +367,7 @@ Page({
               }else {
                 that.setData({
                   showThird: true,
+                  hiddens:true,
                   up_imgs: up_imgs,
                   forscreen_char: forscreen_char,
                   hiddens: true,
@@ -452,6 +461,7 @@ Page({
         },
         success: function (res) {
           that.setData({
+            hiddens:true,
             showThird: true,
             showTpBt: false,
             forscreen_id: forscreen_id,
