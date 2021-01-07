d94a01172d0de0d8b8db1e1e7dec3c1cdd98a5ed
zhangyingtao75144 <125929019@qq.com>
2021-01-07 14:03:28 +0800

    主干版4.6.18

--- scene/pages/welcome/add.js
+++ scene/pages/welcome/add.js
@@ -8,6 +8,7 @@ const utils = require('../../../utils/util.js')
 var mta = require('../../../utils/mta_analysis.js')
 const app = getApp()
 var api_v_url = app.globalData.api_v_url;
+var oss_upload_url = app.globalData.oss_upload_url;
 var openid;
 var box_mac;
 var type;
@@ -18,7 +19,7 @@ Page({
    */
   data: {
     welcome_info:{'img_list':[],'welcome_message':'','font_style_id':0,'font_size_id':0,'font_color':0,'stay_time':0},
-
+    oss_url:app.globalData.oss_url,
     statusBarHeight: getApp().globalData.statusBarHeight,
     SystemInfo: getApp().SystemInfo,
 
@@ -33,6 +34,16 @@ Page({
     openid = options.openid;
     box_mac = options.box_mac;
     type   = options.type;
+    wx.request({
+      url: api_v_url + '/Index/getOssParams',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: function (rest) {
+        that.setData({policy:rest.data.policy,signature:rest.data.signature}) 
+      }
+    })
+    
     utils.PostRequest(api_v_url + '/aa/bb', {//获取欢迎词配置
       openid:openid,
       box_mac:box_mac,
@@ -62,9 +73,23 @@ Page({
   /**
    * 上传欢迎词图片
    */
-  uploadImage:function(e){
+  uploadOnePic: function (e) {
+    app.sleep(200);
+    console.log(e)
     var that = this;
+    var keys = e.currentTarget.dataset.keys;
     var type = e.currentTarget.dataset.type;
+
+    
+    var total_pic = that.data.welcome_info.img_list.length;
+      //var choose_num = 6 - total_pic;
+    
+    if (total_pic > 6) {
+      app.showToast('最多上传6张照片');
+      return false;
+    }
+
+
     wx.showLoading({
       title: '图片上传中...',
       mask: true
@@ -73,7 +98,7 @@ Page({
       addDisabled: true
     })
     wx.chooseImage({
-      count: 6, // 默认6
+      count: 1, // 默认9
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
@@ -82,10 +107,13 @@ Page({
         var index1 = filename.lastIndexOf(".");
         var index2 = filename.length;
         var timestamp = (new Date()).valueOf();
+
         var postf = filename.substring(index1, index2); //后缀名\
         var postf_t = filename.substring(index1, index2); //后缀名
         var postf_w = filename.substring(index1 + 1, index2); //后缀名
+
         var img_url = timestamp + postf;
+
         wx.request({
           url: api_v_url + '/Index/getOssParams',
           headers: {
@@ -110,25 +138,37 @@ Page({
                 OSSAccessKeyId: app.globalData.oss_access_key_id,
                 sucess_action_status: "200",
                 signature: signature
+
               },
+
               success: function (res) {
-                var card_info  = that.data.card_info;
-                if (type == 1) {//头像
-                  var header_url = "forscreen/resource/" + img_url
-                  card_info.header_url = header_url;
-                } else if (type == 2) {//微信二维码
-                  var wx_qrcode = "forscreen/resource/" + img_url
-                  card_info.wx_qrcode = wx_qrcode;
+                var dish_img_url = "forscreen/resource/" + img_url
+                if (type == 'one') {
+                  var dish_img_list = that.data.welcome_info.img_list;
+                  for (var i = 0; i < dish_img_list.length; i++) {
+                    if (i == keys) {
+                      dish_img_list[i] = dish_img_url;
+                      break;
+                    }
+                  }
+                  var welcome_info = that.data.welcome_info;
+                  welcome_info.img_list = dish_img_list;
+                  that.setData({
+                    welcome_info: welcome_info
+                  })
                 } 
-                that.setData({card_info:card_info})
+
                 wx.hideLoading();
+
                 setTimeout(function () {
                   that.setData({
                     addDisabled: false
                   })
                 }, 1000);
               },
-              fail: function ({errMsg}) {
+              fail: function ({
+                errMsg
+              }) {
                 wx.hideLoading();
                 app.showToast('图片上传失败，请重试')
                 that.setData({
@@ -144,11 +184,134 @@ Page({
             })
           }
         })
-      },fail:function(e){
+      },
+      fail: function (e) {
         wx.hideLoading();
         that.setData({
           addDisabled: false
         })
+      }
+    })
+  },
+  uploadImage: function (e) {
+    var that = this;
+    var type = e.currentTarget.dataset.type;
+    var total_pic = that.data.welcome_info.img_list.length;
+    console.log('type'+type)
+    
+    var choose_num = 6 - total_pic;
+    if (total_pic >= 6) {
+      app.showToast('最多上传6张照片');
+      return false;
+    }
+
+
+    wx.showLoading({
+      title: '图片上传中...',
+      mask: true
+    })
+    that.setData({
+      addDisabled: true,
+      //upDisabled: true
+    })
+
+    wx.chooseImage({
+      count: choose_num, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        var tempFilePaths = res.tempFilePaths; //多张图片临时地址
+        var flag = tempFilePaths.length + total_pic;
+  
+        wx.request({
+          url: api_v_url + '/Index/getOssParams',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          success: function (rest) {
+            var policy = rest.data.policy;
+            var signature = rest.data.signature;
+
+            for (var i = 0; i < tempFilePaths.length; i++) {
+              var filename = tempFilePaths[i];
+
+              var index1 = filename.lastIndexOf(".");
+              var index2 = filename.length;
+              var timestamp = (new Date()).valueOf();
+
+              var postf = filename.substring(index1, index2); //后缀名
+              var postf_t = filename.substring(index1, index2); //后缀名
+              var postf_w = filename.substring(index1 + 1, index2); //后缀名
+
+              var img_url = timestamp + postf;
+              //console.log(img_url)
+              that.upOss(filename, postf_w, img_url, policy, signature, i, flag, type)
+              app.sleep(1)
+            }
+          },
+          fail: function (rt) {
+            wx.hideLoading();
+            that.setData({
+              addDisabled: false,
+              //upDisabled: false
+            })
+
+          }
+        })
+      },
+      fail: function (e) {
+        wx.hideLoading();
+        that.setData({
+          addDisabled: false,
+          //upDisabled: false
+        })
+      }
+    })
+  },
+  upOss: function (filename, postf_w, img_url, policy, signature, i, flag, type) {
+
+    var that = this;
+
+
+    wx.uploadFile({
+      url: oss_upload_url,
+      filePath: filename,
+      name: 'file',
+      header: {
+        'Content-Type': 'image/' + postf_w
+      },
+      formData: {
+        Bucket: "redian-produce",
+        name: img_url,
+        key: "forscreen/resource/" + img_url,
+        policy: policy,
+        OSSAccessKeyId: app.globalData.oss_access_key_id,
+        sucess_action_status: "200",
+        signature: signature
+
+      },
+      success: function (res) {
+        if (type == 'all') {
+          var dish_img_list = that.data.welcome_info.img_list;
+          dish_img_list.push("forscreen/resource/" + img_url);
+
+          var end_flag = dish_img_list.length
+        }
+        if (end_flag == flag) {
+          if (type == 'all') {
+            var welcome_info = that.data.welcome_info
+            welcome_info.img_list = dish_img_list;
+            that.setData({
+              welcome_info: welcome_info
+            }, () => {
+              wx.hideLoading();
+              that.setData({
+                addDisabled: false,
+                upDisabled: false
+              })
+            })
+          } 
+        }
       }
     })
   },
