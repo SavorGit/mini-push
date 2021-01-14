314d84a291000a7bc81e29d7bd2283a2152dce2e
zhangyingtao75144 <125929019@qq.com>
2021-01-14 12:16:30 +0800

    主干版4.6.18

--- scene/pages/business/card/add.js
+++ scene/pages/business/card/add.js
@@ -69,7 +69,7 @@ Page({
       addDisabled: true
     })
     wx.chooseImage({
-      count: 6, // 默认6
+      count: 1, // 默认6
       sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
       sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
       success: function (res) {
