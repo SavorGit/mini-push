7e8c9d581db0e91174cccfa000b20b5f232953b0
zhangyingtao75144 <125929019@qq.com>
2021-01-22 11:37:04 +0800

    该谁喝酒

--- games/pages/gamelist.js
+++ games/pages/gamelist.js
@@ -66,6 +66,11 @@ Page({
       url: jumpUrl,
     })
   },
+  gotoDrink:function(e){
+    wx.navigateTo({
+      url: '/pages/activity/turntable/index?openid=' + openid + '&box_mac=' + box_mac 
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
