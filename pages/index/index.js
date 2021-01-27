94e122b4fc2aa8a5d0d6f8042c2df02a3108d919
zhangyingtao75144 <125929019@qq.com>
2021-01-27 15:54:07 +0800

    主干版4.6.20

--- pages/index/index.js
+++ pages/index/index.js
@@ -1272,14 +1272,4 @@ Page({
       }
     }
   },
-  gotoPd:function(e){
-    wx.navigateTo({
-      url: '/pages/scene/business/downloadfile',
-    })
-  },
-  gotoTT:function(e){
-    wx.navigateTo({
-      url: '/games/pages/activity/luckyturn/join?openid=ofYZG4yZJHaV2h3lJHG5wOB9MzxE&box_mac=00226D584281',
-    })
-  }
 })
